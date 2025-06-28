f1a3fae01ffc29bdc73ead965d6adb68ef3fce93
Michele W Sequeira <miche@alum.mit.edu>
2025-06-27 20:47:48 -0600

    Fetching working. Now I need to render it.

--- src/components/TeamPlayersList.jsx
+++ src/components/TeamPlayersList.jsx
@@ -8,7 +8,7 @@ function TeamPlayersList() {
         <h2>List of team players for a game</h2>
         <p>Game date and time here. The teams and games need to be passed down as props.</p>
         <p>The players and subs for this team, game and time listed here.</p>
-        <GetData />
+        <GetData /> 
     </div>
   )
 }
