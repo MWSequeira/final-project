e727233668c24014e82636d4408f2bb46c51a778
Michele W Sequeira <miche@alum.mit.edu>
2025-07-03 17:22:59 -0600

    Working on Subs page

--- src/components/AddSubPage.tsx
+++ src/components/AddSubPage.tsx
@@ -1,5 +1,6 @@
 import type { GameType, PlayerType } from '../App'
 import { Container, Col, Row } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 
 type AddSubPageProps = {
   gameSched: GameType[],
@@ -7,6 +8,11 @@ type AddSubPageProps = {
 }
 
 function AddSubPage( { gameSched, allPlayers }: AddSubPageProps) {
+
+  const chooseGame = (MouseEvent:HTMLButtonElement) => {
+    console.log("Game Chosen")
+    console.log(gameId)
+  }
 
   return (
     <div className='addSubPage'>
@@ -16,12 +22,13 @@ function AddSubPage( { gameSched, allPlayers }: AddSubPageProps) {
         <Row>
           <h1>Add a Sub</h1>
         </Row>
+
         <Row>
           <Col>
             <h2>Games</h2>
             <p><b>Choose Game:</b></p>
             {gameSched.map(game => <div key={game.gameId}>
-              <p>Game {game.gameId} on {game.date} at {game.time}</p>
+              <p><Button>Game {game.gameId}</Button> {game.date} at {game.time}</p>
             </div>)}
           </Col>
           <Col>
@@ -30,6 +37,26 @@ function AddSubPage( { gameSched, allPlayers }: AddSubPageProps) {
               {allPlayers.map(player => <p>
                 {player.firstName} {player.lastName}, {player.position}
               </p>)}
+          </Col>
+          <Col>
+                <Form>
+                  {['checkbox', 'radio'].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                      <Form.Check // prettier-ignore
+                        type={type}
+                        id={`game-${type}`}
+                        label={`game ${type}`}
+                      />
+
+                      <Form.Check
+                        disabled
+                        type={type}
+                        label={`disabled ${type}`}
+                        id={`disabled-default-${type}`}
+                      />
+                    </div>
+                  ))}
+                </Form>
           </Col>
         </Row>
 
